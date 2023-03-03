3832708ba23f2b6bdc6fd02904fb5839cdfac4a4
Fseijo <fseijowork@gmail.com>
2023-03-03 09:40:49 +0100

    main changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("rentrez un chiffre");
+
+        int number = scanner.nextInt();
+
+        System.out.println("rentrez un chiffre");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Result = " + (number1 + number));
+
     }
 }
